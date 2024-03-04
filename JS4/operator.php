2bc200c591f4b5d82618fd0e7690f8d9d705a1a0
adamsafril19 <adamsafrila0@gmail.com>
2024-03-04 12:18:48 +0700

    Pertemuan 04 - Praktikum 03

--- JS4/operator.php
+++ JS4/operator.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 $a = 10;
 $b = 5;
 
@@ -8,3 +8,59 @@ $hasilkali = $a * $b;
 $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
+echo "Hasil Tambah = ". $hasilTambah ."<br>";
+echo "Hasil Kurang = ". $hasilkurang ."<br>";
+echo "Hasil Kali = ". $hasilkali ."<br>";
+echo "Hasil Bagi = ". $hasilBagi ."<br>";
+echo "Sisa Bagi = ". $sisaBagi ."<br>";
+echo "Hasil pangkat = ". $pangkat ."<br>";
+echo "<br><br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+echo "Apakah hasilnya sama: ". $hasilSama ."<br>";
+echo "Apakah hasilnya tidak sama: ". $hasilTidakSama ."<br>";
+echo "Apakah hasilnya lebih kecil: ". $hasilLebihKecil ."<br>";
+echo "Apakah hasilnya lebih besar: ". $hasilLebihBesar ."<br>";
+echo "Apakah hasilnya lebih kecil samadengan: ". $hasilLebihKecilSama ."<br>";
+echo "Apakah hasilnya lebih besar samadengan: ". $hasilLebihBesarSama ."<br>";
+echo "<br><br>";
+
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+echo "Hasil operasi AND = ". $hasilAnd ."<br>";
+echo "Hasil operasi OR = ". $hasilOr ."<br>";
+echo "Hasil operasi NOT a = ". $hasilNotA ."<br>";
+echo "Hasil operasi NOT b = ". $hasilNotB ."<br>";
+echo "<br><br>";
+
+$a += $b;
+echo "Hasil += ". $a ."<br>";
+$a -= $b;
+echo "Hasil -= ". $a ."<br>";
+$a *= $b;
+echo "Hasil *= ". $a ."<br>";
+$a /= $b;
+echo "Hasil /= ". $a ."<br>";
+$a %= $b;
+echo "Hasil %= ". $a ."<br>";
+echo "<br><br>";
+
+$hasilIdentik = $a === $b;
+$hasilTidakIdentik = $a !== $b;
+echo "Hasil Identik = ". $hasilIdentik ."<br>";
+echo "Hasil Tidak Identik = ". $hasilTidakIdentik ."<br>";
+echo "<br><br>";
+
+$jumlahKursi = 45;
+$jumlahPelanggan = 28;
+$sisaKursiKosong = $jumlahKursi - $jumlahPelanggan;
+$persentase = $sisaKursiKosong / $jumlahPelanggan * 100;
+echo "Persentase kursi yang masih kosong adalah ". $persentase ."%";
+?>
