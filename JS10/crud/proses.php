6978c304f21fac8e69ed625a5fa1f6a753c048db
adamsafril19 <adamsafrila0@gmail.com>
2024-04-22 09:12:34 +0700

    Pertemuan 10 - Praktikum 03

--- JS10/crud/proses.php
+++ JS10/crud/proses.php
@@ -17,20 +17,20 @@ if($aksi == 'tambah') {
     }
 }
 
-// elseif ($aksi == 'ubah') {
-//     if (isset($_POST['id'])) {
-//         $id = $_POST['id'];
-//         $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id = $id";
-//         if (mysqli_query($koneksi, $query)) {
-//             header("Location: index.php");
-//             exit();
-//         } else {
-//             echo "Gagal mengupdate data: " . mysqli_error($koneksi);
-//         }
-//     } else {
-//         echo "ID tidak valid.";
-//     }
-// }
+elseif ($aksi == 'ubah') {
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id = $id";
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
+}
 
 // elseif ($aksi == 'hapus') {
 //     if (isset($_GET['id'])) {
